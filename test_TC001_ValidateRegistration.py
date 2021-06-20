91cc9321139a19bf000577489a7be01984e22fc2
Anshul <anshul.swarnkar1990@gmail.com>
2021-06-20 18:06:21 +0530

    first time commit

--- test_TC001_ValidateRegistration.py
+++ test_TC001_ValidateRegistration.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+
 import pytest
 
 def test_registration_validate():
-
-    driver=webdriver.Chrome(ChromeDriverManager().install())
-
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://www.thetestingworld.com/testings/")
     driver.maximize_window()
